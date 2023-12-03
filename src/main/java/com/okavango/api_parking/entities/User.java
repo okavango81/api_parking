2c2f193374441a1e7d6d90156dae85d89c695e43
okavango81 <eguilhermeleitedev81@gmail.com>
2023-12-03 13:07:17 -0300

    created the User entity and the ROLE enum

---
+++ src/main/java/com/okavango/api_parking/entities/User.java
@@ -0,0 +1,48 @@
+package com.okavango.api_parking.entities;
+
+import com.okavango.api_parking.enums.Role;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.io.Serializable;
+import java.time.LocalDateTime;
+import java.util.Objects;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "users")
+public class User implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true, length = 150)
+    private String username;
+
+    @Column(nullable = false, length = 200)
+    private String password;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false, length = 25)
+    private Role role;
+    private LocalDateTime dateOfCreation;
+    private LocalDateTime dateOfModification;
+    private String createdBy;
+    private String modifiedBy;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return Objects.equals(id, user.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}
---
+++ src/main/java/com/okavango/api_parking/enums/Role.java
@@ -0,0 +1,5 @@
+package com.okavango.api_parking.enums;
+
+public enum Role {
+    ROLE_ADMIN, ROLE_CLIENT
+}
