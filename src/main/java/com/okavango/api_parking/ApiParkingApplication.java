b286a959f782c66f4d11ace89c48b42e055c95d8
okavango81 <eguilhermeleitedev81@gmail.com>
2023-12-03 11:48:51 -0300

    created

---
+++ src/main/java/com/okavango/api_parking/ApiParkingApplication.java
@@ -0,0 +1,13 @@
+package com.okavango.api_parking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiParkingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiParkingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/okavango/api_parking/ApiParkingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.okavango.api_parking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiParkingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
